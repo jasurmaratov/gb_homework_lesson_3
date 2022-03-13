0b1def286d10bef6ae1aed39d6fa88294a59c69d
Jasur <jasur-maratov93@mail.ru>
2022-03-13 19:03:42 +0300

    ask

--- jasur_maratov_dz_1.py
+++ jasur_maratov_dz_1.py
@@ -1,1 +1,22 @@
-def num_translate():
+num_list = {
+    'zero': 'ноль',
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четире',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+}
+def num_translate(num_world):
+    return num_list.get(num_world)
+
+def num_transtalen(num_world):
+    to_key = num_list.get(num_world.lower())
+
+    if to_key:
+        return to_key.capitalize() if num_world[0].isupper() else to_key
+
+    return None
